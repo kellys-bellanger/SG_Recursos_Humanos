9ec2abeab40a149aa78d0321bbd545f804553ab7
Sara Ruiz <svruiz@uamv.edu.ni>
2026-06-18 10:45:28 -0600

    feat: agregar validaciones

--- RH.sql
+++ RH.sql
@@ -1,3 +1,10 @@
+use master
+go
+if exists(select * from sys.databases where name like 'SG_Recursos_Humanos')
+begin
+	drop database SG_Recursos_Humanos
+end
+go
 Create database SG_Recursos_Humanos
 go
 
